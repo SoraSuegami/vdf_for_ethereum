36ffdeb07e88a0b7cbb9fcd3d6a56a29073c04dc
SoraSuegami <suegamisora@gmail.com>
2021-08-13 02:05:53 +0900

    remove ether-keystore.

--- src/keccak256.rs
+++ src/keccak256.rs
@@ -5,7 +5,8 @@ use curv::cryptographic_primitives::hashing::traits::Hash;
 use curv::elliptic::curves::traits::ECPoint;
 use curv::elliptic::curves::traits::ECScalar;
 use curv::BigInt;
-use ethereum_keystore::keccak256;
+use sha3::Digest;
+use sha3::Keccak256 as Sha3Keccak256;
 use hex;
 use std::error::Error;
 use std::fmt;
@@ -19,7 +20,7 @@ impl Hash for Keccak256 {
         for value in big_ints {
             inputs.extend_from_slice(&BigInt::to_bytes(value));
         }
-        let result = keccak256(&inputs);
+        let result = _keccak256(&inputs);
         BigInt::from_bytes(&result[..])
     }
 
@@ -28,13 +29,22 @@ impl Hash for Keccak256 {
         for value in ge_vec {
             inputs.extend_from_slice(&value.pk_to_key_slice());
         }
-        let result_bytes = keccak256(&inputs);
+        let result_bytes = _keccak256(&inputs);
         let result_int = BigInt::from_bytes(&result_bytes[..]);
         ECScalar::from(&result_int)
     }
 
     fn create_hash_from_slice(byte_slice: &[u8]) -> BigInt {
-        let result = keccak256(byte_slice);
+        let result = _keccak256(byte_slice);
         BigInt::from_bytes(&result[..])
     }
+}
+
+const KECCAK256_BYTES: usize = 32;
+fn _keccak256(data: &[u8]) -> [u8; KECCAK256_BYTES] {
+    let mut keccak = Sha3Keccak256::new();
+    keccak.update(data);
+    let mut out = [0u8; KECCAK256_BYTES];
+    out.copy_from_slice(&keccak.finalize()[..]);
+    out
 }
